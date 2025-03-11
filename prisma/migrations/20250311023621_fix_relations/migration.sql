af8a94101d9af4549cb6d24fe1e677cc1eea38a5
OLi-byte <douglasso1206@gmail.com>
2025-03-11 13:47:07 -0300

    fix friends relation

--- prisma/migrations/20250311023621_fix_relations/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX `friendships_userId_friendId_idx` ON `friendships`(`userId`, `friendId`);
-
--- CreateIndex
-CREATE INDEX `friendships_friendId_userId_idx` ON `friendships`(`friendId`, `userId`);
